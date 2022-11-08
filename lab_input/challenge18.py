976b1351eaf07ba5f8ecb204321f53470d84ce8e
damon-m-deng <damon.m.deng@gmail.com>
2022-11-08 18:42:29 +0000

    join and split

---
+++ lab_input/challenge18.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+## An input asking the user's name
+user_name = input("What is your name? ")
+## An input asking what day of the week it is
+day_of_the_week = input("What is the day of the week? ")
+## A print statement that reads: Hello, <name>! Happy <day of the week>!
+print("Hello,", user_name, end="! ")
+print("Happy", day_of_the_week, end="!\n")
+## No white space in output
