6ec88b2242636e083e79bad6dd95dd1792f9014a
damon-m-deng <damon.m.deng@outlook.com>
2022-11-15 20:13:42 +0000

    json to python

---
+++ json/py-to-json/makejson01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import json
+
+def main():
+    hitchhikers = [
+            {"name": "Zaphod Beeblebrox", "species": "Betelgeusian"},
+            {"name": "Arthur Dent", "species": "Human"}
+                ]
+
+    print(hitchhikers)
+
+    with open("galaxyguide.json","w") as zfile:
+        # json.dump(python_data, file like obj) returns a json string
+        json.dump(hitchhikers, zfile)
+
+if __name__ == "__main__":
+    main()
